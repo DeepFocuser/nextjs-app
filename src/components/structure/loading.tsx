56f008a7c4c52be5fea862a86e6dd1baaa2375f7
JONGGON <rlawhdrhs27@gmail.com>
2023-07-31 02:49:33 +0900

    update

--- src/components/structure/loading.tsx
+++ src/components/structure/loading.tsx
@@ -1,6 +1,6 @@
 export default function Loading() {
     return (
-        <div className="absolute right-0 flex h-3/5 w-screen flex-col items-center justify-center gap-2">
+        <div className="absolute right-0 flex h-2/5 w-screen flex-col items-center justify-center gap-2">
             <span className="loading loading-spinner loading-lg text-info"></span>
             <p className="text-center">⏳ 잠시만 기다려주세요~ ⌛</p>
         </div>
